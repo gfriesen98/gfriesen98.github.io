ada57b149e367ada6c62e69cd7cbf2a5c09d37c8
Garett Friesen <garettf999@gmail.com>
2020-04-06 22:29:54 -0500

    Updated styles

--- js/timedate.js
+++ js/timedate.js
@@ -1,21 +1,55 @@
-var time,today,doweek,date,currentHour,timeElement,dataElement,dayElement;
-var mS = ['january', 'febuary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
-var dow = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
-today = new Date();
-date = mS[today.getMonth()]+' '+today.getDate() +' '+ today.getFullYear();
-currentHour = today.getHours();
-doweek = dow[today.getDay()]
+function getDateTime(){
+  let time,today,doweek,date,currentHour,timeElement,dataElement,dayElement;
+  let mS = ['january', 'febuary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+  let dow = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+  let isam;
 
-if(currentHour > 12){
-	currentHour -= 12;
-	time = currentHour + ":" + (today.getMinutes()<10?'0':'') + today.getMinutes() + "pm";
-}else{
-	time = today.getHours() + ":" + today.getMinutes() + "am";
+  today = new Date();
+  date = mS[today.getMonth()]+' '+today.getDate() +' '+ today.getFullYear();
+  currentHour = today.getHours();
+  doweek = dow[today.getDay()]
+
+  if(currentHour > 12){
+    currentHour -= 12;
+    time = currentHour + ":" + (today.getMinutes()<10?'0':'') + today.getMinutes() + "pm";
+    isam = true;
+  }else{
+    time = today.getHours() + ":" + today.getMinutes() + "am";
+    isam = false;
+  }
+
+  if(isam){
+    document.getElementById("greeting").innerHTML = "good afternoon";
+  } else {
+    document.getElementById("greeting").innerHTML = "good morning";
+  }
+
+  dataElement = document.getElementById("date");
+  dataElement.innerHTML = doweek + ", " + date;
+
+  timeElement = document.getElementById("time");
+  timeElement.innerHTML = time;
 }
 
-dataElement = document.getElementById("date");
-dataElement.innerHTML = date;
+function getWeather(){
+  let key = '38cfb61dceeb2a0b49157d5c4fae246e';
+  let winnipegId = '6183235';
+  let url = `https://api.openweathermap.org/data/2.5/weather?id=${winnipegId}&appid=${key}`
 
-timeElement = document.getElementById("time");
-timeElement.innerHTML = time + " - " + doweek;
+  fetch(url).then((response) => {
+    return response.json()
+  }).then((data) => {
+    console.log(data);
+    setWeather(data);
+  }).catch((error) => {
+    console.log(error);
+  })
+}
+
+function setWeather(data){
+  let cels = " "+Math.round(parseFloat(data.main.temp)-273.15);
+  document.getElementById('curr').innerHTML = data.weather[0].description + cels + '&deg;C';
+}
+
+window.onload = () => {getWeather(); getDateTime()};
