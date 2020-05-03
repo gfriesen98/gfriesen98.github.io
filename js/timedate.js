f99f6b7a6d5e7a6a2a43446aeb2eec0ac48d92cb
Garett Friesen <garettf999@gmail.com>
2020-05-02 23:14:45 -0500

    Updated styles

--- js/timedate.js
+++ js/timedate.js
@@ -36,35 +36,11 @@ function getDateTime(){
     isam = false;
   }
 
-  // if(isam){
-  //   document.getElementById("greeting").innerHTML = "good afternoon";
-  // } else {
-  //   document.getElementById("greeting").innerHTML = "good morning";
-  // }
-
   dataElement = document.getElementById("date");
   dataElement.innerHTML = doweek + ", " + date;
 
   timeElement = document.getElementById("time");
   timeElement.innerHTML = time;
-}
-
-function setGreeting(){
-  let a = '605886050-fpw5g88DWfiqrox1mpyOjwFBX41BgxJlDooBpPJT';
-  let aa = 'kHtR76KQbW8PQnhXcCPUkCvDZfOhk2u2HnuEB5AYIjdmJ';
-  let key = 'IrBHrFg8CjIDHEo1bU4BY16BJ';
-  let secret = 'URGlofTcMVy3R0WM91MzbfpnvTlBJNb399hLuB7Hwx9pGQi2if';
-  var Twit = require('twit')
-  var T = new Twit({
-    consumer_key: key,
-    consumer_secret: secret,
-    app_only_auth: true
-  })
-
-  T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-    console.log(data)
-  })
-
 }
 
 function getWeather(){
@@ -85,8 +61,8 @@ function getWeather(){
 }
 
 function setWeather(data){
-  let cels = " "+Math.round(parseFloat(data.main.temp)-273.15);
-  document.getElementById('curr').innerHTML = data.weather[0].description + cels + '&deg;C';
+  let cels = ", "+Math.round(parseFloat(data.main.temp)-273.15);
+  document.getElementById('curr').innerHTML = data.weather[0].main + cels + '&deg;C';
 }
 
 window.onload = () => {getWeather()};
